fd2aa9b8cbbc0f2239c549e105c9d7a4b022699e
assopri <i4G4fyaa>
2021-01-22 18:30:47 +0200

    props done!!!

--- src/components/TwootItem.vue
+++ src/components/TwootItem.vue
@@ -4,12 +4,12 @@
             
         <br >
         <div class="twoot-item__user">
-            @{{username}}
+            @{{username}} {{twoot.id}}
         </div>
         <div class="twoot-item__content">
             @{{twoot.content}}
         </div>   
-        <div v-if="favourited"  class="twoot-item__favourited">
+        <div v-if="state.favourited"  class="twoot-item__favourited">
             favourited
         </div>   
             
@@ -18,34 +18,47 @@
   </div>
 </template>
 <script>
-
+import {reactive} from "vue";
       export default {
           name: "TwootItem",
-          props:{
-              username: {
-                  type: String,
-                  required: true
-              },
-              twoot:{
-                  type: Object,
-                  required: true
-              }
-          },
-          data(){
-              return {
-                  favourited : false
-              }
-          },
-          methods:
-          {
-            favouriteTwoot(id)
-            {          
-                let msg = (this.favourited?"de":"") + 'favourite';
-                
-                this.$emit('favourite', id, msg)
-                this.favourited = !this.favourited;
-                this.props.twoot.content +="1";
+          props: {
+            twoot : {
+                type: Object,
+                required: true
+            },
+            username: {
+                type: String,
+                required: true
+                }
+        },
+          setup(props,ctx)
+            {
+               
+                console.log(props)
+                const state = reactive({favourited:false});
+
+                function favouriteTwoot(id)
+                        {       
+                            console.log(`keyprop is ${props.twoot.id}`)   
+                            let msg = (state.favourited?"de":"") + 'favourite';
+                            
+                            ctx.emit('favourite', id, msg)
+                            state.favourited = !state.favourited;
+                            // props.twoot.content +="1";
+                        }
+
+                        return {favouriteTwoot, state };
             }
-          }
+        //   props:{
+        //       username: {
+        //           type: String,
+        //           required: true
+        //       },
+        //       twoot:{
+        //           type: Object,
+        //           required: true
+        //       }
+        //   },
+         
       }
       </script>
